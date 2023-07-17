9a38c3431f2060a613549b6cb2a19a09161b9d81
Kuldeep Kumar <kuldeep.enact@gmail.com>
2023-07-17 18:30:23 +0530

    added validator

--- validator/validationRule.js
+++ validator/validationRule.js
@@ -2,13 +2,12 @@ const { body, oneOf, validationResult } = require('express-validator');
 const apiResponse = require('../utills/response');
 
 
-
-
-export function validate  (req, res, next){
-  const errors = validationResult(req);
-  if (errors.isEmpty()) {
-    return next();
-  }
+exports.validate = (req, res, next) => {
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+      return next();
+    }
+   
  
   return apiResponse.ValidationErrorWithData(res, 'Please Enter Required Field', errors.array());
 
